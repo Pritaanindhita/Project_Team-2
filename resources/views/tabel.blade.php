c2790381513a1f2d4e5964bb2bac86516561f1b9
Prita Anindhita <105988270+Pritaanindhita@users.noreply.github.com>
2022-06-04 22:29:57 +0700

    Tabel buat menampilkan hasil inspeksi

---
+++ resources/views/tabel.blade.php
@@ -0,0 +1,80 @@
+<div class="tabel-inspeksi">
+    <h4>Tabel Hasil Inspeksi</h4>
+    <table class="table table-bordered table-responsive mt-5">
+        <thead class="text-center">
+            <tr>
+                <th rowspan="2">Nomor</th>
+                <th rowspan="2">Workshop</th>
+                <th rowspan="2">Tanggal</th>
+                <th rowspan="2">Nama</th>
+                <th rowspan="2">Pekerjaan</th>
+                <th rowspan="2">Helm</th>
+                <th colspan="5">Kondisi</th>
+                <th rowspan="2">Sepatu Safety</th>
+                <th colspan="2">Kondisi</th>
+                <th rowspan="2">Seragam</th>
+                <th colspan="2">Kondisi</th>
+                <th rowspan="2">Sarung Tangan</th>
+                <th colspan="2">Kondisi</th>
+                <th rowspan="2">Kap Las</th>
+                <th colspan="3">Kondisi</th>
+                <th rowspan="2">Apron</th>
+                <th colspan="2">Kondisi</th>
+                <th rowspan="2">Masker Respirator (Las/Koken)</th>
+                <th colspan="3">Kondisi</th>
+            </tr>
+            <tr>
+                <th>Shell</th>
+                <th>Chin Strap</th>
+                <th>Suspension</th>
+                <th>Expired</th>
+                <th>Stiker</th>
+                <th>Steal Toe Cap</th>
+                <th>Out Sol</th>
+                <th>Kain</th>
+                <th>Relektor</th>
+                <th>Shell</th>
+                <th>Suspension</th>
+                <th>Kaca</th>
+                <th>Apron Dada</th>
+                <th>Apron Lengan</th>
+                <th>Catridge</th>
+                <th>Headband</th>
+                <th>Mask</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
